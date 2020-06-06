7e39221ce0c6575a4a0e273ec6401b9cd8828226
Yanxian Lin <yanxian@umail.ucsb.edu>
2020-06-06 12:56:23 -0700

    TST/BugFix: IT IS WORKING!!!

--- EEGStreamProcessor.py
+++ EEGStreamProcessor.py
@@ -4,14 +4,37 @@ This script retrieves Kafka EEG stream and produces predicted results
 Author:
     Yanxian Lin, Insight Health Data Science Fellow, Boston 2020
 """
-
-from EEGStreamer import KALFK_BROKER_ADDRESS, CONSUMER_TOPIC, STREAMER_TOPIC, sleep_and_sync
+import pickle
 from confluent_kafka import Producer, Consumer, KafkaError
-from time import time, sleep
+from collections import deque
+from time import time
+from EEGStreamer import KALFK_BROKER_ADDRESS, CONSUMER_TOPIC, STREAMER_TOPIC, \
+    sleep_and_sync, decode
+from model import bin_power_avg
+
+DEBUG = True
+
+
+class StreamerOptions:
+    def __init__(self):
+
+        # Streaming related configs
+        self.max_stream_duration = 1000
+        """maximum duration in seconds"""
+        self.streaming_rate = 1
+        """streaming rate in Hz"""
+        self.delay_refresh_intv = 1/self.streaming_rate
+        """refresh interval in seconds"""
+
+        # Data related configs
+        self.sampling_rate = 400
+        """data sampling rate in Hz"""
+        self.data_shape = [22, self.sampling_rate]  # nchannel x nsamples
+        """shape of data"""
 
 
 class EEGStreamProcessor:
-    def __init__(self):
+    def __init__(self, so: StreamerOptions):
         self.consumer = Consumer({
                 'bootstrap.servers': KALFK_BROKER_ADDRESS,
                 'auto.offset.reset': 'earliest',
@@ -24,72 +47,142 @@ class EEGStreamProcessor:
         self.producer = Producer({'bootstrap.servers': KALFK_BROKER_ADDRESS})
         """producer that produces predition results"""
 
-        self.samping_rate = 400
-        """processing rate in Hz"""
-
-        self.delay_refresh_intv = 1.0  # time in second
+        self.streaming_rate = so.streaming_rate
+        """streaming rate in Hz"""
+        self.delay_refresh_intv = so.delay_refresh_intv
         """refresh interval in seconds"""
-
-        self.max_stream_duration = 1000
+        self.max_stream_duration = so.max_stream_duration
         """maximum duration in seconds"""
 
-        self.data_shape = [22, 400]  # nchannel x nsamples
+        self.data_shape = so.data_shape
         """shape of data"""
+        self.sampling_rate = so.sampling_rate
+        """data sampling rate in Hz"""
 
-        self.__streamqueue = []
+        self.__streamqueue = deque()
         """queue for stream"""
-
-        self.__dataqueue = []
+        self.__data = deque()
         """queue for data"""
-
-        self.__predicted_result = None
+        self.__data_t = deque()
+        """queue of data timestamp"""
+        self.__pdata = deque()
+        """queue of processed_data"""
+        self.__pdata_t = deque()
         """"""
+        self.__res = deque()
+        """"""
+        self.__res_t = deque()
+        """"""
+        self.__model = None
+        """"""
+        self.setup()
+
+    def setup(self):
+        self.consumer.subscribe([STREAMER_TOPIC])
+        with open('model.pkl', 'rb') as fp:
+            self.model = pickle.load(fp)
 
     def start(self):
-        self.consumer.subscribe([STREAMER_TOPIC])
+        """Start streamer"""
 
         start_time = time()
-        wall_clock = time()
-        sampling_delay = 0.8 / self.samping_rate
-        sampling_count = 1
+        heart_beat = time()
+        stream_delay = 0.8 / self.streaming_rate
+        stream_count = 1
 
-        for isamp in range(0, max(1, int(self.max_stream_duration))):
+        nsamp = max(1, int(self.max_stream_duration * self.streaming_rate))
+        for isamp in range(0, nsamp):
 
+            print(f'Sample: {isamp}/{nsamp}') if DEBUG else None
             self.read_in()
             self.preprocess()
             self.predict()
             self.publish()
 
-            sampling_delay, sampling_count, wall_clock = sleep_and_sync(
-                sampling_delay, sampling_count, wall_clock, self.samping_rate,
-                self.delay_refresh_intv, DEBUG=True)
+            stream_delay, stream_count, heart_beat = sleep_and_sync(
+                stream_delay, stream_count, heart_beat,
+                self.streaming_rate, self.delay_refresh_intv, DEBUG=DEBUG)
 
             # too long, shut down
             if time() - start_time > self.max_stream_duration:
                 break
 
     def read_in(self):
-        """read stream from Kafka
+        """read stream from Kafka and append to streamqueue
 
         Returns:
             list of list: dataset (nchannel x nsample) or None
         """
-        msgs = self.consumer.consume(num_messages=self.samping_rate)
-        if msgs is not None and len(msgs) > 0:
-            print('I got some msgs!')
-            # TODO:
-        raise NotImplementedError
+        chunk_size = self.data_shape[1]
+        msgs = []
+        while chunk_size > 100:
+            msgs.extend(self.consumer.consume(num_messages=100, timeout=1))
+            chunk_size -= 100
+        msgs.extend(self.consumer.consume(num_messages=chunk_size, timeout=1))
+
+        print(f"msg = {msgs}") if DEBUG else None
+
+        if msgs is None or len(msgs) <= 0:
+            return None
+
+        self.__streamqueue.extendleft(msgs)  # Enqueue
+
+        if len(self.__streamqueue) < self.data_shape[1]:
+            return None
+
+        # Dequeue
+        msgs__ = [self.__streamqueue.pop() for i in range(0, self.data_shape[1])]
+
+        timestamps, data = [], []
+        for msg in msgs__:
+            time, values = decode(msg.key(), msg.value())
+            timestamps.append(time) if time is not None else None
+            data.append(values) if time is not None else None
+        #TODO:// assert there is not big time gap in the data
+
+        if len(data) < self.data_shape[1]:
+            return None
+
+        print(timestamps[0], data[0]) if DEBUG else None
+
+        data = tuple(zip(*data))
+        self.__data.append(data)
+        self.__data_t.append(timestamps[0])
+
+        print(f"INFO: Sucessfully Read a chunk") if DEBUG else None
 
     def preprocess(self):
         """preprocess data"""
-        raise NotImplementedError
+        if len(self.__data) <= 0:
+            self.__data.clear(), self.__data_t.clear()
+            return None
+        data = [self.__data.pop() for i in range(0, len(self.__data))]
+        time = [self.__data_t.pop() for i in range(0, len(self.__data_t))]
+
+        X = bin_power_avg(data, fsamp=self.sampling_rate)
+        self.__pdata.extendleft(X)
+        self.__pdata_t.extendleft(time)
 
     def predict(self):
-        raise NotImplementedError
+        if len(self.__pdata) <= 0:
+            return None
+        for i in range(0, len(self.__pdata)):
+            processed_data = self.__pdata.pop()
+            processed_t = self.__pdata_t.pop()
+            predicted_rels = self.model.predict([processed_data])
+            self.__res.appendleft(predicted_rels[0])
+            self.__res_t.appendleft(processed_t)
 
     def publish(self):
         """publish predicted result"""
-        raise NotImplementedError
+        for i in range(0, len(self.__res)):
+            res = self.__res.pop()
+            tim = self.__res_t.pop()
+            joint_str = res
+            key = 'key'
+            value = "{'t':%.6f,'v':["%float(tim)+"'"+joint_str+"'"+"]}"
+            self.producer.produce(CONSUMER_TOPIC, key=key, value=value)
+            print(f'Published: {tim}, {res}') if DEBUG else None
 
     def stop(self):
         self.consumer.close()
@@ -97,6 +190,6 @@ class EEGStreamProcessor:
 
 
 if __name__ == '__main__':
-    esp = EEGStreamProcessor()
+    esp = EEGStreamProcessor(StreamerOptions())
     esp.start()
     esp.stop()
--- EEGStreamer.py
+++ EEGStreamer.py
@@ -5,8 +5,10 @@ Step 1 and Step 2 in https://kafka.apache.org/quickstart
 1. run ```bin/zookeeper-server-start.sh config/zookeeper.properties```
 2. run ```bin/kafka-server-start.sh config/server.properties```
 """
+import ast
 from confluent_kafka import Producer, Consumer, KafkaError
 from time import time, sleep
+from datetime import datetime
 import numpy as np
 
 KALFK_BROKER_ADDRESS = 'localhost:9092'  # TODO: Implement this
@@ -23,12 +25,15 @@ DEBUG = 1
 #         print(f"Message produced: {str(msg)}")
 
 
-def sleep_and_sync(sampling_delay,
-                   sampling_count,
-                   wall_clock,
-                   samping_rate,
-                   delay_refresh_intv,
-                   DEBUG=True):
+def decode(key, value):
+    """decode a message key and value and return list"""
+    # TODO: if key is invalid then return None
+    mydata = ast.literal_eval(value.decode("UTF-8"))
+    return mydata['t'], mydata['v']
+
+
+def sleep_and_sync(sampling_delay, sampling_count, wall_clock,
+                   samping_rate, delay_refresh_intv, DEBUG=True):
     """Sleep and adjust the sampling delay
 
     Returns:
@@ -54,14 +59,27 @@ def sleep_and_sync(sampling_delay,
                 raise ValueError
         except ValueError:
             sampling_delay = 0
-            print(
-                "WARNING: NEW DELAY TIME INTERVAL WAS A NEGATIVE NUMBER. Setting to 0..")
+            print("WARNING: NEW DELAY TIME INTERVAL WAS A NEGATIVE NUMBER. Setting to 0..")
         print(f"Deviation: {deviation:.2f} ms, new delay:"
               f" {sampling_delay * 1000:.2f} ms.") if DEBUG else None
         sampling_count = 0
         wall_clock = new_heartbeat
 
     return sampling_delay, sampling_count, wall_clock
+
+
+# class StreamerOptions:
+#     def __init__(self):
+#         self.max_stream_duration = 1000
+#         """maximum duration in seconds"""
+#         self.streaming_rate = 1
+#         """streaming rate in Hz"""
+#         self.delay_refresh_intv = 1.0
+#         """refresh interval in seconds"""
+#         self.data_shape = [22, 4]  # nchannel x nsamples
+#         """shape of data"""
+#         self.sampling_rate = 40
+#         """data sampling rate in Hz"""
 
 
 class EEGStreamer:
@@ -92,13 +110,10 @@ class EEGStreamer:
         # Streaming related configs
         self.max_stream_duration = 1000
         """streaming duration in second"""
-
-        self.samping_rate = 40
-        """samping rate in Hz"""
-
+        self.streaming_rate = 4
+        """Streaming rate in Hz"""
         self.delay_refresh_intv = 1.0  # time in second
         """refresh interval in seconds"""
-
         self.flush_interval = 0.1
         """Flush interval for Producer. In second"""
 
@@ -107,9 +122,10 @@ class EEGStreamer:
         """Interval in second of listening"""
 
         # Data related configs
+        self.sampling_rate = 400
+        """samping rate in Hz"""
         self.nchannel = 22
         """number of channels"""
-
         self.montage = '1020'
         """standard 10-20 montage"""
 
@@ -124,38 +140,32 @@ class EEGStreamer:
         montage = self.montage
 
         start_time = time()
-        wall_clock = time()
-        sampling_delay = 0.8 / self.samping_rate
-        sampling_count = 1
+        heart_beat = time()
+        stream_delay = 0.8 / self.streaming_rate
+        stream_count = 1
 
-        for isample in range(0, np.shape(ds)[1]):
+        for istream in range(0, ds.shape[1]):
 
             # Produce signal as stream
-            joint_str = ','.join(['%.6f'%float(ds[ich,isample]) for ich in
+            joint_str = ','.join(['%.6f'%float(ds[ich,istream]) for ich in
                                   range(0, np.shape(ds)[0])])
-            timestamp = time() - start_time
+            timestamp = time()
             value = "{'t':%.6f,'v':["%float(timestamp)+joint_str+"]}"
             self.producer.produce(self.producer_topic, key=montage, value=value)
 
             # Flush
-            intv = int(self.flush_interval * self.samping_rate)
-            if sampling_count % max(intv, 1) == 0:
+            intv = int(self.flush_interval * self.streaming_rate)
+            if stream_count % max(intv, 1) == 0:
                 self.producer.flush(1)
 
             # Listen
-            intv = int(self.listen_interval * self.samping_rate)
-            if (sampling_count) % max(intv, 1) == 0:
-                listen = self.listen()
-                if listen == 1:
-                    print("!!!!!!SEIZURE IS COMING!!!!!!")
-                elif listen == 2:
-                    print("not sync'ed. check network connection.")
-                else:
-                    print("all good")
+            intv = int(self.listen_interval * self.streaming_rate)
+            if (stream_count) % max(intv, 1) == 0:
+                self.listen()
 
-            sampling_delay, sampling_count, wall_clock = sleep_and_sync(
-                sampling_delay, sampling_count, wall_clock, self.samping_rate,
-                self.delay_refresh_intv, DEBUG=True)
+            stream_delay, stream_count, heart_beat = sleep_and_sync(
+                stream_delay, stream_count, heart_beat,
+                self.streaming_rate, self.delay_refresh_intv, DEBUG=True)
 
             # too long, shut down
             if time() - start_time > self.max_stream_duration:
@@ -167,26 +177,32 @@ class EEGStreamer:
         Returns:
             int: 1=alert. 0=background. 2=not sync.
         """
+        # if listen == 1:
+        #     print(timestamp, "!!!!!!SEIZURE IS COMING!!!!!!")
+        # elif listen == 2:
+        #     print(timestamp, "not sync'ed. check network connection.")
+        # else:
+        #     print(timestamp, "all good")
         msg = self.consumer.poll(0.1)
         if msg is None:
-            return 0
-
+            pass
         elif not msg.error():
-            if DEBUG:
-                print('Received message: {0}'.format(msg.value()))
-            #TODO:// Implement This
-            return 0
-
+            print('Received message: {0}'.format(msg.value())) if DEBUG else None
+            # TODO: if msg.key() == XXX
+            t, v = decode(msg.key(), msg.value())
+            t = datetime.fromtimestamp(int(t))
+            # TODO: Check time stamp
+            if v[0] == 'pres':
+                print(t, "!!!!!!SEIZURE IS COMING!!!!!!")
+            elif v[0] == 'bckg':
+                print(t, "all good")
+            else:
+                print(t, f'UNKNOWN: {v[0]} not recognized')
         elif msg.error().code() == KafkaError._PARTITION_EOF:
-            if DEBUG:
-                print('End of partition reached {0}/{1}'.format(msg.topic(),
-                                                                 msg.partition()))
-            return 2
-
+            print('End of partition reached {0}/{1}'\
+                  .format(msg.topic(),msg.partition())) if DEBUG else None
         else:
-            if DEBUG:
-                print('Error occured: {0}'.format(msg.error().str()))
-            return -1
+            print('Error occured: {0}'.format(msg.error().str())) if DEBUG else None
 
     def stop(self):
         # consume the remaining msg and stop the consumer
@@ -197,7 +213,5 @@ class EEGStreamer:
 
 if __name__ == '__main__':
     eegstreamer = EEGStreamer()
-    start_time = time()
-    # eegstreamer.consumer_topic = eegstreamer.producer_topic
     eegstreamer.start()
     eegstreamer.stop()
