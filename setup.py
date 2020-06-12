9aa8b8c0adf16622edeb12a6675a5c16ddb3af50
Yanxian Lin <yanxian@umail.ucsb.edu>
2020-06-12 11:55:11 -0700

    Added new files and directory structure from cookiecutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='ReReal-time forecasting epileptic seizure using electroencephalogram',
+    author='Yanxian Lin',
+    license='MIT',
+)
